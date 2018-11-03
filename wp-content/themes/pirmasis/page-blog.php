3090fe899f7597ed825a1b4d28686750bfe9a391
Arturas <arturas.danilevicius@gmail.com>
2018-11-04 00:28:11 +0200

    css config, menu responsiv

---
+++ wp-content/themes/pirmasis/page-blog.php
@@ -0,0 +1,29 @@
+<?php /*
+Template Name: blog
+Description: A Page Template for custom page.
+*/
+get_header(); ?>
+
+
+
+	<div id="main-content" class="visual-container">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-2">
+					<div >
+						<?php get_sidebar(); ?>
+					</div>
+				</div>
+				<div class="col-md-10 container-white">
+					<div class="rte">
+						<div id="wrap-page">
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+<?php get_footer(); ?>
