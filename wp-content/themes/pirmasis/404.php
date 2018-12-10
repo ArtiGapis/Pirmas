d20100796b457cf857671f6b73de8ed159eba9b9
Arturas <arturas.danilevicius@gmail.com>
2018-12-10 09:21:25 +0200

    php tvarkymas

--- wp-content/themes/pirmasis/404.php
+++ wp-content/themes/pirmasis/404.php
@@ -5,8 +5,8 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="not-found">
-                    <h1><?php _e("OH, NO!  Supervillains destroyed this website. Try heading back to our Home page. You will be covered there.", "itbrolis"); ?></h1>
-                    <a href="<?php echo get_home_url(); ?>"><?php _e("Back", "itb"); ?></a>
+                    <h1><?php _e("Atsiprašome, laikinai nepasiekiama"); ?></h1>
+                    <a href="<?php echo get_home_url(); ?>"><?php _e("Atgal"); ?></a>
                 </div>
             </div>
         </div>
